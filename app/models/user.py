cc12551a7a3f70dd5a335d6bfa3ec49f47bf0287
Varun Verma <varun.verma2024@gmail.com>
2025-07-08 19:38:31 +0530

    Update user models with User_PyModel

--- app/models/user.py
+++ app/models/user.py
@@ -1,11 +1,11 @@
-from sqlalchemy import Column, Integer, String, Boolean, ARRAY, Text, TIMESTAMP
+from sqlalchemy import Column, Integer, String, TIMESTAMP
 from sqlalchemy.sql import func
 '''-----------------------'''
-from ..database import Base
+from app.database import Base
 
 class Users(Base):
     __tablename__ = "users"
-    
+
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
