98331e9b6e46b4ee477059c0d28b29027e8269d1
Varun Verma <varun.verma2024@gmail.com>
2025-07-13 11:47:34 +0530

    made changes to model to match new alembic table names

--- app/models/vote.py
+++ app/models/vote.py
@@ -2,7 +2,7 @@ from app.database import Base
 from sqlalchemy import Column, Integer, ForeignKey
 
 class Votes(Base):
-    __tablename__ = "votes_table"
+    __tablename__ = "votes"
 
     user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
     post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
